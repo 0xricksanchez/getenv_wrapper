f3fe2a5cd564de1c32b5c76b9b2c669dd877fcd7
0xricksanchez <admin@0x434b.dev>
2021-11-18 17:27:35 +0100

    only intercept ENV var TZ while making all other calls to getenv() return their proper value

--- src/lib.rs
+++ src/lib.rs
@@ -2,22 +2,60 @@ use libc::c_char;
 use std::ffi::CStr;
 use std::io::{self, Read};
 
-
+/// # Safety
+///
+/// This function tries to intercept the getenv call to the environment variable "TZ"
+/// while returning the set values for all others regularly
 #[no_mangle] 
-pub extern "C" fn getenv(name: *const c_char) -> *const c_char {
-    let name = unsafe { CStr::from_ptr(name) };
-    println!("[+] Intercepted a getenv() call for: {:?}", name);
-    
-    let mut p = vec![0u8; 512];
-    let stdin = io::stdin();
-    let sz = stdin.lock().read(&mut p).unwrap();
-    
-    p.truncate(sz);
-    p.push(b'\0');
-    p.leak().as_ptr().cast()
+pub unsafe extern "C" fn getenv(name: *const c_char) -> *const c_char {
+    let name_str = CStr::from_ptr(name).to_str();
+    if name_str == Ok("TZ") {
+        println!("[+] Intercepted a getenv() call for: {:?}", name_str);
+        let mut p = vec![0u8; 512];
+        let stdin = io::stdin();
+        let sz = stdin.lock().read(&mut p).unwrap();
+        
+        p.truncate(sz);
+        p.push(b'\0');
+        return p.leak().as_ptr().cast();
+    } else if name_str.is_err() {
+        return vec![0u8; 1].leak().as_ptr().cast()
+    } else {
+        println!("[+] Ignoring a getenv() call for: {:?}", name_str);
+        let mut next = libc_environ;
+        while !next.is_null() && !{ *next }.is_null() {
+            let env = CStr::from_ptr(*next);
+            let env_str = env.to_str();
+            let (key, val) =  match env_str {
+                Ok(s) => {
+                    let parts = s.split_once("=");
+                    match parts {
+                        Some(x) => { (x.0, x.1) },
+                        None => { ("", "") }
+                    }
+                },
+                Err(_) => { ("", "") }
+            };
+            
+            if name_str == Ok(key) {
+                return val.as_ptr().cast();
+            } else {
+                next = next.offset(1);
+            }
+        }
+    }
+    vec![0u8; 1].leak().as_ptr().cast()
 }
 
+/// # Safety
+///
+/// This function just calls the nightmarish implementation above, be ready
 #[no_mangle] 
-pub extern "C" fn secure_getenv(name: *const c_char) -> *const c_char {
+pub unsafe extern "C" fn secure_getenv(name: *const c_char) -> *const c_char {
     getenv(name)
+}
+
+extern {
+    #[link_name = "environ"]
+    static libc_environ: *const *const std::os::raw::c_char;
 }
